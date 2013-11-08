ef556f5930c5f4acaeb3a298f0b207f023499bfc
rojas2 <rojas2@email.sc.edu>
2013-11-08 13:05:25 -0500

    Added new page

--- src/com/example/game/MainActivity.java
+++ src/com/example/game/MainActivity.java
@@ -21,4 +21,8 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    public void newGame(View v_choosePet){
+    	
+    }
 }
