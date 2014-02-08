73d300006900127c471fd03804c3d77e3c57102b
huntleja <huntleyjohn1992@gmail.com>
2014-02-08 05:38:47 -0500

    Updated QuestionLoader
    Unimplemented and untested.  Currently should load json file and provide
    at least one question with answers with value of correct answer.  Needs
    update to have multiple questions and implementation into question
    screen to debug.

--- src/com/example/game/QuestionLoader.java
+++ src/com/example/game/QuestionLoader.java
@@ -1,121 +1,154 @@
 package com.example.game;
 
 import java.io.BufferedReader;
-import java.io.IOException;
+
 import java.io.InputStreamReader;
 import java.io.InputStream;
-import java.util.ArrayList;
 
+
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONArray;
 
+
 import android.app.Activity;
-import android.os.Handler;
-import android.content.Intent;
-import android.content.res.Resources;
+//import android.os.Handler;
+//import android.content.Intent;
+//import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 
-/*Class that loads the questions from json file and provides question and answers
- * for question screen.
+/**
+ * QuestionLoader.java
+ * 
+ *Loads current question from json file and provides
+ *methods to return strings of questions, answers, and location
+ *of the correct answer.
+ *
+ *Note: Current version only loads one question from JSON file
+ *		as code has yet to be tested on display.
+ *
  */
 
 public class QuestionLoader extends Activity {
-	
+	private static final String TAG_QUESTIONS = "Questions";
 	private static final String TAG_QUESTION = "Question";
 	private static final String TAG_CORRECTANSWER = "CorrectAnswer";
-	private static final String TAG_ANSWERS = "Answer";
-	private static final String TAG_QUESTIONS = "Questions";
+	private static final String TAG_ANSWERS = "Answers";
+	private static final String TAG_ANSWER = "Answer";
+	
 
 	//Questions and Answer jsonArray
-	private JSONArray questions;
-	private ArrayList<String> questionList = new ArrayList<String>();	
+	private JSONObject aQuestion;
+	private static JSONArray questionList = null;
+	private JSONArray answerList;
 	
-	BufferedReader bReader = null;
-	private static JSONArray quesList = null;
+	private String question;
+	private String answers[];
+    private int correctAnswer;
+    
+	BufferedReader bufferedReader = null;
 	
+	
+	/**
+	 * Instantiation method.
+	 */
 	public void onCreate(Bundle savedInstanceState)  {
+		/*
+		 * May need to use thread/handler.
+		 */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_screen);
-        
-        Thread thread = new Thread() {
-        	public void run() {
-		        try {
-		        	Thread.sleep(3*1000);
-			        finish();
-			        loadQuestions();
-	        	} catch (Exception e) {
-	        	} 
-        	}
-        };
-        thread.start();
-
+			        try {
+						loadQuestions();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
      }    
 	
-	//Class function that loads the json file as a json object
+	/**
+	 * Class function that reads the json file and loads as a json object
+	 * Also, calls method to set question, answers array, and correct answer
+	 * integer.
+	 * 
+	 * @throws Exception
+	 */
     private void loadQuestions() throws Exception {
     	try {
 		InputStream questions = this.getBaseContext().getResources()
 				.openRawResource(R.raw.question);
-		bReader = new BufferedReader(new InputStreamReader(questions));
+		bufferedReader = new BufferedReader(new InputStreamReader(questions));
 		StringBuilder quesString = new StringBuilder();
 		String aJsonLine = null;
-		while ((aJsonLine = bReader.readLine()) != null) {
+		while ((aJsonLine = bufferedReader.readLine()) != null) {
 			quesString.append(aJsonLine);
 		}
 		Log.d(this.getClass().toString(), quesString.toString());
 		JSONObject quesObj = new JSONObject(quesString.toString());
-		quesList = quesObj.getJSONArray("TAG_QUESTIONS");
+		questionList = quesObj.getJSONArray(TAG_QUESTIONS);
+		
+		qaSetter(); 
+		
+		
 		Log.d(this.getClass().getName(),
-				"Num Questions " + quesList.length());
+				"Num Questions " + questionList.length());
     	} catch (Exception e){
     		
     	} finally {
 			try {
-				bReader.close();
+				bufferedReader.close();
 			} catch (Exception e) {
 				Log.e("", e.getMessage().toString(), e.getCause());
 			}
 			}
+       }
+    
+    /**
+     * Setter method that sets the current question and answer 
+     * strings.
+     */
+    private void qaSetter() throws JSONException {
+    	//Question setter from json object
+    	aQuestion = questionList.getJSONObject(0); 
+    	question = aQuestion.getString(TAG_QUESTION);
     	
+    	answerList = aQuestion.getJSONArray(TAG_ANSWERS);
+    	//fills array of answers from corresponding question json object
+    	for(int i = 0; i<=3; i++){
+    		answers[i] = answerList.getJSONObject(i).getString(TAG_ANSWER);
+    	}
+    	
+    	//sets int number from current question
+    	correctAnswer = aQuestion.getInt(TAG_CORRECTANSWER);
+	}
 
+	/**
+	 * Getter method that returns the current set question.
+	 * 
+	 * @return Current question to be displayed
+	 */
+    public String getQuestion(){
+    return question;
     }
-    
-    
-    //Returns Question String
-    public void getQuestion(){
     	
-    }
-    
-    //Method to return Answer String 1
-    public void getAnswer1(){
-    	
-    }
-    
-    //Method to return Answer String 2
-    public void getAnswer2(){
-    	
-    }
-    
-    //Method to return Answer String 3
-	public void getAnswer3(){
-	
-	}
-	
-    //Method to return Answer String 4
-	public void getAnswer4(){
-	
-	}
-	
-	//Method that returns integer of correct answer.
+	/**
+	 * Gets location for the correct answer 
+	 * in the answer array.
+	 * 
+	 * @return current location of correct answer
+	 */
     public int getCorrectAnswer(){
-    return 0;	
+    return correctAnswer;	
     }
     
-    private static JSONArray getQuesList() {
-    	return quesList;
+    /**
+	 * Getter method
+	 * 
+	 * @return An array of answer strings
+	 */
+    public String[] getAnswers(){
+    	return answers;
     }
-	
 	
 
 }
